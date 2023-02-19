172e1f7c885bc591ab4a7ccf88692c61b4714574
Tony W <bobacrypto2@gmail.com>
2023-02-18 17:11:03 -0800

    Jest with matchers

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,20 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
