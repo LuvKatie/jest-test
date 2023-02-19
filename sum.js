11307236d7ec5a27462e0ddf823bf66cb430c5a3
Tony W <bobacrypto2@gmail.com>
2023-02-19 02:05:19 -0800

    Add reverse string to testing | Add calculator object with 4 functions of add, subtract, divide, multiply which successfully tested on jest

--- sum.js
+++ sum.js
@@ -6,7 +6,34 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function reverse(str) {
+    let strSplit = str.split("");
+    let newStr = "";
+    for (let i = (strSplit.length - 1); i >= 0; i--) {
+        newStr += strSplit[i]
+    }
+    
+    return newStr;
+}
+
+const calculator = {
+    add: function(a, b ) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    },
+    multiply: function (a, b) {
+        return a * b;
+    }
+}
+
 module.exports = {
     sum,
     capitalize,
+    reverse,
+    calculator,
 }
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const {sum, capitalize} = require('./sum');
+const {sum, capitalize, reverse, calculator,} = require('./sum');
 // const capitalize = sum.capitalize;
 
 test('adds 1 + 2 to equal 3', () => {
@@ -7,6 +7,26 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('Capitialize first letter of Hello World', () => {
   expect(capitalize("hello world")).toBe("Hello world");
+});
+
+test('Reverse string: Hello', () => {
+  expect(reverse("Hello")).toBe("olleH");
+});
+
+test('Calculator add two integers', () => {
+  expect(calculator.add(2, 4)).toBe(6);
+});
+
+test('Calculator subtract two integers', () => {
+  expect(calculator.subtract(4, 2)).toBe(2);
+});
+
+test('Calculator divide two integers', () => {
+  expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test('Calculator multiply two integers', () => {
+  expect(calculator.multiply(6, 2)).toBe(12);
 });
 
 // test('two plus two is four', () => {
